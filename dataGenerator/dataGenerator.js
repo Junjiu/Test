a35597fee5192086f0b0fdb9d1e3cd9252bdc944
root <root@ip-172-31-33-147.us-east-2.compute.internal>
2019-04-11 22:50:37 +0000

    final part start

--- dataGenerator/dataGenerator.js
+++ dataGenerator/dataGenerator.js
@@ -1,23 +1,125 @@
 function dataGenerator() {
-	this.getData = function() {
-		var data = {};
+	var mongodb = require('mongodb');
+	var MongoClient = mongodb.MongoClient;
+	var url = "mongodb://localhost:27017/";
+
+	var x = 42;
+        var y = -93;
+	var vehicle = {};
+	this.createVehicle = function(name){
+		vehicle[name] = {};
+		vehicle[name]['Lat'] = 42;
+		vehicle[name]['Long'] = -91;
+	};
+	var count = 0;
+	var savedlocations = {};
+	this.getData = function(name, seed) {
+
+		var data = vehicle[name];
 		var dateObj = new Date();
 		var month = dateObj.getUTCMonth() + 1; //months from 1-12
 		var day = dateObj.getUTCDate();
 		var year = dateObj.getUTCFullYear();
 		data['time'] = year + "/" + month + "/" + day;;
-                data['Lat'] = Date.now() % 10 + 42;
-		data['Long'] = ((Date.now() * 2) % 10) - 93;
-		data['Alt'] = Date.now() % 300;
-		data['GPS_head'] = 0;
-		data['X_accel'] = Date.now() % 600;
-		data['Y_accel'] = Date.now() % 600;
-		data['Z_accel'] = Date.now() % 600;
-		data['Acc_mag'] = Date.now() % 1000;
-		data['Conveyor'] = Date.now() % 50;
-		data['Spinner'] = Date.now() % 50;
-		data['Prewet'] = Date.now() % 50;
-		return data;
+		data['Lat'] = data['Lat'] + (Math.random()  *  10) * seed / 1000;
+		data['Lat'] = data['Lat'];
+		data['Long'] = data['Long'] +  (Math.random()  *  10 % 10) * seed  / 1000;
+		data['Long'] = data['Long'];
+		data['Alt'] = Math.random()  * 300 * seed;
+		data['GPS_head'] = Math.random()  * 600 * seed;
+		data['X_accel'] = Math.random()  * 600 * seed;
+		data['Y_accel'] = Math.random()  * 600 * seed;
+		data['Z_accel'] = Math.random()  *600 * seed;
+		data['Acc_mag'] = Math.random()  *1000 * seed;
+		data['Conveyor'] = Math.random()  *50 * seed;
+		data['Spinner'] = Math.random()  *50 * seed;
+		data['Prewet'] = Math.random()  *50 * seed;
+		vehicle[name] = data;
+		MongoClient.connect(url, { useNewUrlParser: true }, function(err, db) {
+			if (err) throw err;
+			var dbo = db.db("record");
+		       	dbo.collection("truck").insertOne(data, function(err, res) {
+					if (err) throw err;
+			       		db.close();	
+			});	
+
+	       });
+	       return data;
 	};
+	this.getRecord = function(callback) {
+		MongoClient.connect(url, { useNewUrlParser: true }, function(err, db) {
+				if (err) throw err;
+				var dbo = db.db("record");
+				dbo.collection("truck").find().toArray(function(e, d){
+					db.close();
+					callback(d);
+				});	
+		});
+	};
+	 this.getRealRecord = function(callback) {
+                MongoClient.connect(url, { useNewUrlParser: true }, function(err, db) {
+                                if (err) throw err;
+                                var dbo = db.db("realRecord");
+                                dbo.collection("record").find().limit(500).toArray(function(e, d){
+                                        db.close();
+                                        callback(d);
+                                });
+                });
+        };
+	  this.getOneRealRecord = function(startCounts, range, callback) {
+                MongoClient.connect(url, { useNewUrlParser: true }, function(err, db) {
+                                if (err) throw err;
+                                var dbo = db.db("realRecord");
+                                dbo.collection("record").find().limit(3000).toArray(function(e, d){
+                                        db.close();
+					var results =[];
+					for(var j = 0; j < startCounts.length; ++j){
+						startCount = startCounts[j];
+						startCount = parseInt(startCount);
+						temp = d.slice(startCount + count%range, startCount + count%range + 4);
+						var result = temp[0];
+						for(var i = 1; i < temp.length; i = i + 1){
+							for(var key in temp[i]){
+								var str = String(temp[i][key]);
+								if(str.length != 0){
+									result[key] = temp[i][key]
+								}
+							}	
+						}
+						console.log(String(savedlocations[j]).length);
+						var locationJSON ={}
+						try{
+							locationJSON = JSON.parse(result['Hanover Park IL TGS - Location ()']);
+							for(var key in locationJSON){
+								result[key] = locationJSON[key];
+							}	
+							savedlocations[j] = locationJSON;
+						}catch(e){
+							console.log("=====");
+							console.log(String(savedlocations[j]).length);
+							console.log('catch exception');
+						 	 for(var key in savedlocations[j]){
+                                                                result[key] = savedlocations[j][key];
+                                                        }	
+						}
+						results.push(result);
+					}	
+					count += 4;
+					callback(results);
+                                });
+		});
+        };
+	 this.getRealRecordByTime = function( startDate, endDate, callback) {
+                MongoClient.connect(url, { useNewUrlParser: true }, function(err, db) {
+                                if (err) throw err;
+                                var dbo = db.db("realRecord");
+				var filter = {'timestamp' : {$gt:startDate, $lt:endDate}};
+                                dbo.collection("record").find(filter).limit(500).toArray(function(e, d){
+                                        db.close();
+                                        callback(d);
+                                });
+                });
+        };
+
 };
 module.exports = dataGenerator;
--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,54 @@ var router = express.Router();
 var dataGenerator = require('../dataGenerator/dataGenerator');
 dataGenerator = new dataGenerator();
 /* GET home page. */
+  dataGenerator.createVehicle("V1");
+  dataGenerator.createVehicle("V2");
+  dataGenerator.createVehicle("V3");
 router.get('/', function(req, res, next) {
 	res.header("Access-Control-Allow-Origin", "*");
 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-
-	res.send(dataGenerator.getData());
+ 	var result = [];
+	result.push(dataGenerator.getData("V1",1));
+	result.push(dataGenerator.getData("V2",2));
+	res.send(result);
 });
 
+router.get('/getOneRealRecord', function(req, res, next) {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        dataGenerator.getOneRealRecord([0,600,1200,1800], 500, (d) =>{
+		res.send(d);
+	});
+});
+
+router.get('/record', function(req, res, next) {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        
+	dataGenerator.getRecord( (d) =>{
+		var ave = 0;
+		  for(var i = 0; i < d.length; ++i){
+                d[i]['total_accel'] = Math.sqrt(d[i]['X_accel']*d[i]['X_accel']
+		+ d[i]['Y_accel']*d[i]['Y_accel'] + d[i]['Z_accel']*d[i]['Z_accel']);
+       		ave += d[i]['total_accel'];
+		d[i]['ave_accel'] = ave /(i+1);	  
+		  }	
+		res.send(d);
+	});
+});
+router.get('/realrecord', function(req, res, next) {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	 dataGenerator.getRealRecord( (d) =>{
+                res.send(d);
+        });
+});
+router.get('/realrecordbytime', function(req, res, next) {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+         dataGenerator.getRealRecordByTime(req.query.startDate, req.query.endDate, (d)  =>{
+                res.send(d);
+        });
+});
 module.exports = router;
+
